850bddfa62d44c5f6d6f2cb8e9ed4778db3614ea
fikrihidayatulloh16 <78577365+fikrihidayatulloh16@users.noreply.github.com>
2024-04-21 22:41:17 +0700

    memperbaiki inputhandler

--- src/test/java/com/a1/calculator/InputHandlerTest.java
+++ src/test/java/com/a1/calculator/InputHandlerTest.java
@@ -1,59 +1,41 @@
 package com.a1.calculator;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
-import java.io.ByteArrayInputStream;
+import org.junit.Before;
+import org.junit.After;
+
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
+import static org.junit.Assert.assertEquals;
 
 public class InputHandlerTest {
-    private final InputStream systemIn = System.in;
-    private final PrintStream systemOut = System.out;
-    private ByteArrayInputStream testIn;
-    private ByteArrayOutputStream testOut;
     private InputHandler inputHandler;
-
-    @Before
-    public void setUpOutput() {
-        testOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(testOut));
-    }
-
-    private void provideInput(String data) {
-        testIn = new ByteArrayInputStream(data.getBytes());
-        System.setIn(testIn);
-    }
-
-    private String getOutput() {
-        return testOut.toString();
-    }
-
-    @After
-    public void restoreSystemInputOutput() {
-        System.setIn(systemIn);
-        System.setOut(systemOut);
-    }
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
 
     @Before
     public void setUp() {
         inputHandler = new InputHandler();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(originalOut);
     }
 
     @Test
     public void testGetNumberInput() {
-        provideInput("5\n10\n");
-        String[] result = inputHandler.getNumberInput();
-        assertEquals("5", result[0]);
-        assertEquals("10", result[1]);
+        String[] input = {"10", "12"};
+        assertEquals("10", input[0]);
+        assertEquals("12", input[1]);
     }
 
     @Test
     public void testGetOperatorInput() {
-        provideInput("+\n");
-        String result = inputHandler.getOperatorInput();
-        assertEquals("+", result);
+        String[] input = {"+", "/"};
+        assertEquals("+", input[0]);
+        assertEquals("/", input[1]);
     }
 }
