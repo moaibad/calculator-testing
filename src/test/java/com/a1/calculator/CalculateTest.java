d5ad84a05b4ec8f99ff7288b49c9ed75f30e6dbc
Rekabriyan <rekabriyanch08@gmail.com>
2024-04-21 23:41:03 +0700

    Fixing CalculateTest

--- src/test/java/com/a1/calculator/CalculateTest.java
+++ src/test/java/com/a1/calculator/CalculateTest.java
@@ -1,37 +1,54 @@
 package com.a1.calculator;
 
 import junit.framework.TestCase;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class CalculateTest extends TestCase {
     
     private Calculate calculate;
-    
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
     @Before
     public void setUp() {
         calculate = new Calculate();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(originalOut);
     }
 
     @Test
     public void testCalculateResult(){
-        // Test penjumlahan dua bilangan positif
+        String expectedOutput;
+                // Test penjumlahan dua bilangan positif
+        expectedOutput = "Hasil: 27";
         calculate.calculateResult(15, 12, "+");
-        Assert.assertEquals("Hasil: 27", "Hasil: 27");
+        Assert.assertTrue(outputStream.toString().contains(expectedOutput));
 
         // Test pengurangan dua bilangan positif
+        expectedOutput = "Hasil: -8";
         calculate.calculateResult(4, 12, "-");
-        Assert.assertEquals("Hasil: -8", "Hasil: -8");
+        Assert.assertTrue(outputStream.toString().contains(expectedOutput));
 
         // Test perkalian dua bilangan positif
+        expectedOutput = "Hasil: 2700";
         calculate.calculateResult(900, 3, "*");
-        Assert.assertEquals("Hasil: 2700", "Hasil: 2700");
+        Assert.assertTrue(outputStream.toString().contains(expectedOutput));
 
         // Test pembagian dua bilangan positif
+        expectedOutput = "Hasil: 38.9";
         calculate.calculateResult(1945, 50, "/");
-        Assert.assertEquals("Hasil: 38.9", "Hasil: 38.9");
+        Assert.assertTrue(outputStream.toString().contains(expectedOutput));
 
-}
+    }
 
 }
